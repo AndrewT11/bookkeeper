be4716daaeb9bce94b4e9652181e40b4cc788e56
Andrew Tran <tranandrew@gmail.com>
2022-05-25 18:30:55 -0500

    removing two imports from bookRoutes.js

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -1,14 +1,12 @@
 const express = require("express");
-const Book = require("../model/Book");
 const router = express.Router();
-const Product = require("../model/Book")
-const bookController = require("../controllers/bookController")
+const bookController = require("../controllers/bookController");
 
 // localhost:5000/books/
-router.get("/", bookController.getAllBooks)
-router.post("/", bookController.addBook)
-router.get("/:id", bookController.getById)
-router.put("/:id", bookController.updateBook)
-router.delete("/:id", bookController.deleteBook)
+router.get("/", bookController.getAllBooks);
+router.post("/", bookController.addBook);
+router.get("/:id", bookController.getById);
+router.put("/:id", bookController.updateBook);
+router.delete("/:id", bookController.deleteBook);
 
 module.exports = router;
