7f536c525e5576bdd262932556055a75ee83cb90
Andrew Tran <tranandrew@gmail.com>
2022-05-25 14:16:59 -0500

    book fetch setup to pull book info from MongoDB

--- frontend/src/components/Book/Books.js
+++ frontend/src/components/Book/Books.js
@@ -1,7 +1,18 @@
 //This file will render all books from Book.js here.
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+
+const URL = "http://localhost:5000/books";
+
+const fetchHandler = async () => {
+  return await axios.get(URL).then((res)=> res.data);
+}
 
 const Books = () => {
+  const [books, setBooks] =useState();
+  useEffect(()=> {
+    fetchHandler().then(data=>setBooks(data))
+  }, [])
   return (
     <div>All your Books right hurr</div>
   )
