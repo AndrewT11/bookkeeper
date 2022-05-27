c89ae55cf20dc56944b717d2219663237893db58
Andrew Tran <tranandrew@gmail.com>
2022-05-27 17:45:21 -0500

    moved around import files to look cleaner

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,23 @@
 // bookkeeping MongoDB admin password: FffvnsNC9mLN21Lj
 const express = require("express");
-const mongoose = require("mongoose")
-const router = require("./routes/bookRoutes")
-const cors = require('cors')
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const router = require("./routes/bookRoutes");
+
 const app = express();
 
 // Middlewares
-app.use(express.json()) // will convert middleware and allow reading of json everywhere through server
+app.use(express.json()); // will convert middleware and allow reading of json everywhere through server
 app.use(cors()); // helps in overriding securities
-app.use("/books", router) //localhost:5000/books
+app.use("/books", router); //localhost:5000/books
 
-mongoose.connect(
-    "mongodb+srv://admin:FffvnsNC9mLN21Lj@cluster0.yntfo.mongodb.net/?retryWrites=true&w=majority")
-    .then(()=>console.log("Connected to Database"))
-    .then(()=> { app.listen(5000)})
-    .catch((err)=>(err))
+mongoose
+  .connect(
+    "mongodb+srv://admin:FffvnsNC9mLN21Lj@cluster0.yntfo.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Connected to Database"))
+  .then(() => {
+    app.listen(5000);
+  })
+  .catch((err) => err);
