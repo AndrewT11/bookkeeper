70b628a9aee8da2792708595674029bc617ba266
Andrew Tran <tranandrew@gmail.com>
2022-05-27 19:17:43 -0500

    apply CSS alignment of text displayed. npm i @mui, react-router-dom, @mui-icons for home pc

--- frontend/src/components/Book/Book.js
+++ frontend/src/components/Book/Book.js
@@ -1,19 +1,20 @@
-import React from 'react'
-import { Button } from '@mui/material';
+import React from "react";
+import { Button } from "@mui/material";
+import "./Book.css";
 
 const Book = (props) => {
-    const { id, name, author, description, price, image } = props.book;
+  const { id, name, author, description, price, image } = props.book;
   return (
-    <div>
-        <img src={image} alt={name} />
-        <article>By {author}</article>
-        <h3>{name}</h3>
-        <p>{description}</p>
-        <h2>${price}</h2>
-        <Button>Update</Button>
-        <Button>Delete</Button>
+    <div className="bookCard">
+      <img src={image} alt={name} />
+      <article>By {author}</article>
+      <h3>{name}</h3>
+      <p>{description}</p>
+      <h2>${price}</h2>
+      <Button>Update</Button>
+      <Button>Delete</Button>
     </div>
-  )
-}
+  );
+};
 
 export default Book;
--- frontend/src/components/Book/Books.js
+++ frontend/src/components/Book/Books.js
@@ -1,30 +1,32 @@
 //This file will render all books from Book.js here.
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import Book from "./Book"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Book from "./Book";
+import "./Book.css";
 
 const URL = "http://localhost:5000/books";
 
 const fetchHandler = async () => {
-  return await axios.get(URL).then((res)=> res.data);
-}
+  return await axios.get(URL).then((res) => res.data);
+};
 
 const Books = () => {
-  const [books, setBooks] =useState();
-  useEffect(()=> {
-    fetchHandler().then(data=>setBooks(data.books))
-  }, [])
+  const [books, setBooks] = useState();
+  useEffect(() => {
+    fetchHandler().then((data) => setBooks(data.books));
+  }, []);
   return (
     <div>
       <ul>
-        {books && books.map((book, i) => (
-          <div key={i}>
-            <Book book={book}/>
-          </div>
-        ))}
+        {books &&
+          books.map((book, i) => (
+            <li key={i}>
+              <Book book={book} />
+            </li>
+          ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Books
+export default Books;
