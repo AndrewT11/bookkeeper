94451c273d22101f8ab07acdd1469cf5833e3c10
Andrew Tran <tranandrew@gmail.com>
2022-06-02 19:40:01 -0500

    deleteHandler created. Book deleted, but not off browser until reload. Debug

--- frontend/src/components/Book/Book.js
+++ frontend/src/components/Book/Book.js
@@ -1,11 +1,21 @@
 import React from "react";
 import { Button } from "@mui/material";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Book.css";
+import axios from "axios";
 
 const Book = (props) => {
+  const history = useNavigate;
   // id marked as _id for MongoDB. MongoDB will auto assign ID
   const { _id, name, author, description, price, image } = props.book;
+
+  const deleteHandler = async () => {
+    await axios
+      .delete(`http://localhost:5000/books/${_id}`)
+      .then((res) => res.data)
+      .then(() => history("/books"));
+  };
+
   return (
     <div className="bookCard">
       <img src={image} alt={name} />
@@ -24,7 +34,7 @@ const Book = (props) => {
         >
           Update
         </Button>
-        <Button sx={{ mt: "auto" }} color="error">
+        <Button onClick={deleteHandler} sx={{ mt: "auto" }} color="error">
           Delete
         </Button>
       </aside>
