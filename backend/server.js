becad3fef496bb47fcdca1a2fce1f4944a550f9d
Andrew Tran <tranandrew@gmail.com>
2022-06-05 16:41:54 -0500

    heroku deployment...hopefully

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
 const cors = require("cors");
+
+// Step specifically added for Heroku
+const path = require("path");
 
 const router = require("./routes/bookRoutes");
 
@@ -13,12 +19,26 @@ app.use(express.json()); // will convert middleware and allow reading of json ev
 app.use(cors()); // helps in overriding securities
 app.use("/books", router); //localhost:5000/books
 
+// also in Heroku deploy docs
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+//added in Heroku deployment docs
+// Step 1. Will import the client build folder to the server
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+// Step 2. ensure that the routes defined with React Router are working once the application has been deployed. Handles any request by redirecting them to index.html.
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
+
 mongoose
-  .connect(
-    "mongodb+srv://admin:FffvnsNC9mLN21Lj@cluster0.yntfo.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then(() => console.log("Conneezeed to the Deezy"))
-  .then(() => {
-    app.listen(5000);
+  .connect(process.env.MONGODB_CONNECTION_STRING, {
+    useNewUrlPraser: true,
+    useUnifiedToplogy: true,
   })
+  .then(() => console.log("Conneezeed to the Deezy"))
   .catch((err) => err);
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
