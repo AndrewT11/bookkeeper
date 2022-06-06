483bc4204524bcede94db533a5620919af9b9d37
Andrew Tran <tranandrew@gmail.com>
2022-06-06 11:27:57 -0500

    remove { path: ./config.env } from require dotenv

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 // const mongoose = require("mongoose");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config();
 const cors = require("cors");
 const path = require("path");
 const db = require("./config/connection");
