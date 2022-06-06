76da1433718ea3e12944223a2163b4330eab01f8
Andrew Tran <tranandrew@gmail.com>
2022-06-05 20:19:56 -0500

    required path

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const router = require("./routes/bookRoutes");
 
