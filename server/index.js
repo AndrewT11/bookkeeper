3ad7a442d36293267ab0ea9323a58a435f240c68
Andrew Tran <tranandrew@gmail.com>
2022-06-05 20:06:41 -0500

    heroku json and server modification

--- server/index.js
+++ server/index.js
@@ -5,12 +5,26 @@ const cors = require("cors");
 
 const router = require("./routes/bookRoutes");
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
 // Middlewares
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // will convert middleware and allow reading of json everywhere through server
 app.use(cors()); // helps in overriding securities
 app.use("/books", router); //localhost:5000/books
+
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  res.sendFile(path.join(__dirname, "../client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 mongoose
   .connect(
@@ -18,6 +32,6 @@ mongoose
   )
   .then(() => console.log("Conneezeed to the Deezy"))
   .then(() => {
-    app.listen(5000);
+    app.listen(PORT);
   })
   .catch((err) => err);
